7d597bad3914577aebee65f76c6692d4872b0539
haithamaleryani <haithamaleryani00@gmail.com>
2025-06-15 19:56:01 +0300

    add gsap and do some to animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
 
 function App() {
- 
+  gsap.registerPlugin(useGSAP);
+  const icon = useRef(null);
+
+  useGSAP(() => {
+    gsap.to(".logo", {
+      rotation: "+=360",
+      duration: 2,
+      repeat: 3,
+
+      yoyo: true,
+    });
+  });
 
   return (
     <>
-      <div className='flex'>
+      <div className="">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo" alt="React logo" />
         </a>
       </div>
-     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
